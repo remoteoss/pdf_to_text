e6b6351e8d049c9b5e81d81c19f364ac94558a60
Philipp Tessenow <philipp@remote.com>
2022-04-28 14:53:44 +0200

    extract error stringification

--- native/pdf_to_text/src/lib.rs
+++ native/pdf_to_text/src/lib.rs
@@ -4,7 +4,7 @@ use rustler::Binary;
 
 #[rustler::nif]
 fn from_path(path: String) -> Result<String, String> {
-    pdf_extract::extract_text(path).map_err(|e| format!("{}",e))
+    pdf_extract::extract_text(path).map_err(stringify_error)
 }
 
 #[rustler::nif]
@@ -12,10 +12,14 @@ fn from_content(content: Binary) -> Result<String, String> {
     let mut s = String::new();
     {
         let mut output = PlainTextOutput::new(&mut s);
-        let doc = Document::load_mem(content.as_slice()).map_err(|e| format!("{}", e))?;
-        output_doc(&doc, &mut output).map_err(|e| format!("{}", e))?;
+        let doc = Document::load_mem(content.as_slice()).map_err(stringify_error)?;
+        output_doc(&doc, &mut output).map_err(stringify_error)?;
     }
     Ok(s)
+}
+
+fn stringify_error<Displayable: std::fmt::Display>(e: Displayable) -> String {
+    format!("{}", e)
 }
 
 rustler::init!("Elixir.PdfToText", [

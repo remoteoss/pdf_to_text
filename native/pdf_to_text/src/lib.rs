4cc035e1bfc64374e34e7964360744a66b315400
Philipp Tessenow <philipp@remote.com>
2022-05-07 01:39:27 +0200

    use poppler

--- native/pdf_to_text/src/lib.rs
+++ native/pdf_to_text/src/lib.rs
@@ -1,24 +1,32 @@
-use pdf_extract::{self, PlainTextOutput, output_doc};
-use lopdf::Document;
+use poppler::Document;
 use rustler::Binary;
+use std::fs;
 
 #[rustler::nif]
 fn from_path(path: String) -> Result<String, String> {
-    pdf_extract::extract_text(path).map_err(stringify_error)
+    let data = fs::read(path).map_err(stringify)?;
+    to_text(&data)
 }
 
 #[rustler::nif]
 fn from_content(content: Binary) -> Result<String, String> {
-    let mut s = String::new();
-    {
-        let mut output = PlainTextOutput::new(&mut s);
-        let doc = Document::load_mem(content.as_slice()).map_err(stringify_error)?;
-        output_doc(&doc, &mut output).map_err(stringify_error)?;
-    }
-    Ok(s)
+    to_text(content.as_slice())
 }
 
-fn stringify_error<Displayable: std::fmt::Display>(e: Displayable) -> String {
+fn to_text(data: &[u8]) -> Result<String, String> {
+    let document = Document::from_data(data, None).map_err(stringify)?;
+    let mut out = String::new();
+    for i in 0..document.n_pages() {
+        if let Some(page) = document.page(i) {
+            if let Some(text) = page.text() {
+                out = out + &text + "\n\n;"
+            }
+        }
+    }
+    Ok(out)
+}
+
+fn stringify<Displayable: std::fmt::Display>(e: Displayable) -> String {
     format!("{}", e)
 }
 

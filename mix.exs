bb4b7c2e005bee1703c106af2f19e4ca52637169
Filipe <filipe@remote.com>
2022-05-18 16:01:01 +0100

    Update version to 0.0.1 to match release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PdfToText.MixProject do
   def project do
     [
       app: :pdf_to_text,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       package: package(),

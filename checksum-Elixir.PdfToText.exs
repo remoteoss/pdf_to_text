f950bb4813ec7104884e9b958756a5d6b05a93e2
Filipe <filipe@remote.com>
2022-05-18 19:00:22 +0100

    Add checksum files

---
+++ checksum-Elixir.PdfToText.exs
@@ -0,0 +1,8 @@
+%{
+  "libpdf_to_text-v0.0.1-nif-2.14-x86_64-apple-darwin.so.tar.gz" => "sha256:93573ea824187fbff6fb42821ffeafdc9e6eb515e96b3183f2ad55faf8a9e70e",
+  "libpdf_to_text-v0.0.1-nif-2.14-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:9cae4175f41c06c295e30d60bb8d2a24c173ef6787cbc28bd2469749b2171feb",
+  "libpdf_to_text-v0.0.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:8cd4e2b496132fbea75fb962986627af56ecd2a8773b95307d56ff4c8829d840",
+  "libpdf_to_text-v0.0.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:73e77e684c4e9124623a19eb9182af2005411de2eb46d5d0fdf5c72b14599132",
+  "libpdf_to_text-v0.0.1-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:f5f4528dd23544b12c74a499d6149dc21b8d7de455a6370c5f7fc8f15477a35e",
+  "libpdf_to_text-v0.0.1-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:509ebff02b501068eb04324fac15dda3a6d2e167a12a5c4b6a9b8297a3e48408",
+}
